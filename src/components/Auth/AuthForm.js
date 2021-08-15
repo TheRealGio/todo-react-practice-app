55353781379d3431e426eb3a0d159ae8b5915114
TheRealGio <1142343@student.birzeit.edu>
2021-08-15 16:15:12 +0300

    Form is now a component, Trimmed CSS file

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import classes from "./AuthForm.module.css";
 import Button from "../UI/Button/Button";
 import InputComponent from "../UI/InputComponent/InputComponent";
+import FormComponent from "../FormComponent/FormComponent";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -11,49 +12,41 @@ const AuthForm = () => {
   };
 
   return (
-    <main className={classes.auth}>
-      <form>
-        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-        {!isLogin && (
-          <InputComponent
-            type="text"
-            id="name"
-            placeholder="John Smith"
-            required
-          >
-            Your Name
-          </InputComponent>
-        )}
-        <InputComponent
-          type="email"
-          id="email"
-          placeholder="john.smith@email.com"
-          required
-        >
-          Your Email
+    <FormComponent headerText={isLogin ? "Login" : "Sign Up"}>
+      {!isLogin && (
+        <InputComponent type="text" id="name" placeholder="John Smith" required>
+          Your Name
         </InputComponent>
-        <InputComponent type="password" id="password" required>
-          Password
+      )}
+      <InputComponent
+        type="email"
+        id="email"
+        placeholder="john.smith@email.com"
+        required
+      >
+        Your Email
+      </InputComponent>
+      <InputComponent type="password" id="password" required>
+        Password
+      </InputComponent>
+      {!isLogin && (
+        <InputComponent type="password" id="password-confirm" required>
+          Confirm Password
         </InputComponent>
-        {!isLogin && (
-          <InputComponent type="password" id="password-confirm" required>
-            Confirm Password
-          </InputComponent>
-        )}
-        <p>Forgot Password?</p>
-        <Button type="button" className={classes["btn-transition"]}>
-          Login
-        </Button>
-        <h5>OR</h5>
-        <Button
-          type="button"
-          className={classes["btn-transition"]}
-          onClick={switchAuthModeHandler}
-        >
-          {isLogin ? "New User? Register!" : "Go Back"}
-        </Button>
-      </form>
-    </main>
+      )}
+      <p>Forgot Password?</p>
+      <Button type="button" className={classes["btn-transition"]}>
+        Login
+      </Button>
+      <h5>OR</h5>
+      <Button
+        type="button"
+        className={classes["btn-transition"]}
+        onClick={switchAuthModeHandler}
+      >
+        {isLogin ? "New User? Register!" : "Go Back"}
+      </Button>
+    </FormComponent>
   );
 };
 
---
+++ src/components/FormComponent/FormComponent.js
@@ -0,0 +1,15 @@
+import { checkPropTypes } from "prop-types";
+import classes from "./FormComponent.module.css";
+
+const FormComponent = (props) => {
+  return (
+    <main className={classes['form-ui']}>
+      <form>
+        <h1>{props.headerText}</h1>
+        {props.children}
+      </form>
+    </main>
+  );
+};
+
+export default FormComponent;
